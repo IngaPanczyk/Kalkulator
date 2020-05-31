030e989659b85f63d327451b2cf47c7bb42af38f
IngaPanczyk <ingawaaa@gmail.com>
2020-05-31 21:43:39 +0200

    Zmiany 2020-05-31

--- src/main/java/com/kodilla/Kalkulator.java
+++ src/main/java/com/kodilla/Kalkulator.java
@@ -4,14 +4,16 @@ public class Kalkulator {
     int b = 8;
     int a = 5;
     int c;
-    public void add () {
+
+    public void add() {
         c = a + b;
     }
-    public void subtract () {
+
+    public void subtract() {
         c = a - b;
     }
 
-    public String toString(){
+    public String toString() {
         return "wynik " + c;
     }
 
